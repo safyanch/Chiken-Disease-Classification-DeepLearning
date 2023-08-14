e07c8f1a2e577b4c068f97ba78842a4a14294d8c
Dr. M. Safyan <safyanch@gmail.com>
2023-08-14 14:48:08 +0500

    after pasting full project

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chiken-Disease-Classification-DeepLearning"
-AUTHOR_USER_NAME = "safyanch"
+REPO_NAME = "Chicken-Disease-Classification--Project"
+AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "safyanch@gmail.com"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
 
 
 setuptools.setup(
